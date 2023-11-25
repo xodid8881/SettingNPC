da5f0971bf835379f4346f1e27f742e5f2961b5b
Hwa_Baeg_ <aoadid8881@naver.com>
2023-11-26 05:27:04 +0900

    Update NPCClick.php

--- SettingNPC/src/SettingNPC/EventListener/NPCClick.php
+++ SettingNPC/src/SettingNPC/EventListener/NPCClick.php
@@ -33,36 +33,45 @@ class NPCClick implements Listener
             if ($entity->getNameTag() != null){
                 if (isset($this->npcdb [$entity->getNameTag()])){
                     $event->cancel();
-                    $command = $this->npcdb [$npcname] ["Command"];
-                    if (! isset ( $this->chat [$damager->getName ()] )) {
-                        $this->chat [$damager->getName ()] = date("YmdHis",strtotime ("+3 seconds"));
-                        return true;
-                    }
-                    
-                    $CoolTime = $this->npcdb [$npcname] ["CoolTime"];
-                    $Cool = $this->npcdb [$npcname] ["Cool"];
-                    
-                    if (date("YmdHis") - $this->chat [$damager->getName ()] < 3) {
-                        $damager->sendMessage ( SettingNPC::TAG . "이용 쿨타임이 지나지 않아 불가능합니다." );
-                        return true;
-                    } else {
-                        $permissions = $this->npcdb [$npcname] ["Permissions"];
-                        /*
-                            ROOT_OPERATOR OP
-                            ROOT_USER USER
-                        */
+                    $command = $this->npcdb [$entity->getNameTag()] ["Command"];
+                    $CoolTime = $this->npcdb [$entity->getNameTag()] ["CoolTime"];
+                    $Cool = (int)$this->npcdb [$entity->getNameTag()] ["Cool"];
+                    if ($CoolTime == "false"){
+                        $permissions = $this->npcdb [$entity->getNameTag()] ["Permissions"];
                         if ($permissions == "ROOT_OPERATOR"){
                             if (!$damager->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
                                 $damager->sendMessage(SettingNPC::TAG . "권한이 없습니다.");
                                 return true;
                             }
                             Server::getInstance()->getCommandMap ()->dispatch ( $damager, $command );
-                            $this->chat [$damager->getName ()] = date("YmdHis",strtotime ("+3 seconds"));
                             return true;
                         } else if ($permissions == "ROOT_USER"){
                             Server::getInstance()->getCommandMap ()->dispatch ( $damager, $command );
-                            $this->chat [$damager->getName ()] = date("YmdHis",strtotime ("+3 seconds"));
                             return true;
+                        }
+                    } else if ($CoolTime == "true"){
+                        if (! isset ( $this->chat [$damager->getName ()] )) {
+                            $this->chat [$damager->getName ()] = date("YmdHis",strtotime ("+{$Cool} seconds"));
+                            return true;
+                        }
+                        if (date("YmdHis") - $this->chat [$damager->getName ()] < {$Cool}) {
+                            $damager->sendMessage ( SettingNPC::TAG . "이용 쿨타임이 지나지 않아 불가능합니다." );
+                            return true;
+                        } else {
+                            $permissions = $this->npcdb [$entity->getNameTag()] ["Permissions"];
+                            if ($permissions == "ROOT_OPERATOR"){
+                                if (!$damager->hasPermission(DefaultPermissions::ROOT_OPERATOR)) {
+                                    $damager->sendMessage(SettingNPC::TAG . "권한이 없습니다.");
+                                    return true;
+                                }
+                                Server::getInstance()->getCommandMap ()->dispatch ( $damager, $command );
+                                $this->chat [$damager->getName ()] = date("YmdHis",strtotime ("+{$Cool} seconds"));
+                                return true;
+                            } else if ($permissions == "ROOT_USER"){
+                                Server::getInstance()->getCommandMap ()->dispatch ( $damager, $command );
+                                $this->chat [$damager->getName ()] = date("YmdHis",strtotime ("+{$Cool} seconds"));
+                                return true;
+                            }
                         }
                     }
                 }
